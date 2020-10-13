1cfd91cdb508a3549b32ec93a6225ef375e828d3
y-i <y-i@users.noreply.github.com>
2020-10-13 16:07:06 +0900

    add QuicTransport server and client for room

--- client/room.js
+++ client/room.js
@@ -3,27 +3,43 @@ class QuicTransportRoom {
    *
    * @param {String} roomname
    * @param {String} BaseURL
+   * @param {Function} cb - Callback function called when client receives data
    */
-  constructor(roomname, BaseURL = 'quic-transport://localhost:4433/') {
+  constructor(roomname, BaseURL = 'quic-transport://localhost:4433/', cb = console.log) {
     this.roomname = roomname;
     this.BaseURL = BaseURL;
+    this.cb = cb;
+
+    this.length = {
+      datagram: 0,
+      stream: 0,
+    };
+    this.buffer = {
+      datagram: new Uint8Array(),
+      stream: new Uint8Array(),
+    };
   }
+  /**
+   * Connect to QuicTransportServer
+   */
   async connect() {
+    if (window.QuicTransport === undefined) {
+      throw new Error('QuicTransport is unsupported');
+    }
+
     try {
       const URL = `${this.BaseURL}${this.roomname}`;
       this.transport = new QuicTransport(URL);
       console.log('Initiating connection...');
     } catch (err) {
-      console.error('Failed to create connection object. ' + err);
-      return;
+      throw new Error('Failed to create connection object. ' + err.message);
     }
 
     try {
       await this.transport.ready;
       console.log('Connection ready.');
     } catch (err) {
-      console.error('Connection failed. ' + err);
-      return;
+      throw new Error('Connection failed. ' + err.message);
     }
 
     this.transport.closed
@@ -50,13 +66,13 @@ class QuicTransportRoom {
           break;
         }
         {
-          const decoder = new TextDecoderStream('utf-8');
-          const reader = value.readable.pipeThrough(decoder).getReader();
-          this.readFromReader(reader, data => data);
+          const decoder = new TextDecoder('utf-8');
+          const reader = value.readable.getReader();
+          this.readFromReader(reader, data => decoder.decode(data), 'stream');
         }
       }
     } catch (err) {
-      console.error('Error while accepting streams:', err);
+      throw new Error('Error while accepting streams:', err.message);
     } finally {
       reader.releaseLock();
     }
@@ -64,10 +80,10 @@ class QuicTransportRoom {
   async readDatagrams() {
     const decoder = new TextDecoder('utf-8');
     const reader = this.transport.receiveDatagrams().getReader();
-    this.readFromReader(reader, data => decoder.decode(data));
+    this.readFromReader(reader, data => decoder.decode(data), 'datagram');
   }
 
-  async readFromReader(reader, decoder) {
+  async readFromReader(reader, decoder, type) {
     try {
       while (true) {
         const { done, value } = await reader.read();
@@ -76,31 +92,83 @@ class QuicTransportRoom {
           console.log('Stream closed', stream);
           return;
         }
-        const data = decoder(value);
-        console.log('Received data on stream:', data, data.length);
+        if (this.length[type] === 0) {
+          const [len, rest] = this.unpack(Uint8Array.from([...this.buffer[type],...value]));
+
+          this.buffer[type] = rest;
+          this.length[type] = len;
+        } else {
+          this.buffer[type] = Uint8Array.from([...this.buffer[type],...value]);
+        }
+
+        if (this.buffer[type].byteLength > this.length[type]) { // split and flush
+          const data = decoder(this.buffer[type].slice(0,this.length[type]));
+          await this.cb(data);
+          this.buffer[type] = this.buffer[type].slice(this.length[type]);
+          this.length[type] = 0;
+        } else if (this.buffer[type].byteLength === this.length[type]) { // flush
+          const data = decoder(this.buffer[type]);
+          await this.cb(data);
+
+          this.buffer[type] = new Uint8Array();
+          this.length[type] = 0;
+        }
       }
     } catch (err) {
-      console.error('Error while reading from stream:', err);
+      console.error(err);
+      throw new Error('Error while reading from stream:', err);
     } finally {
       reader.releaseLock();
     }
   }
 
+  /**
+   * Send data by using datagram
+   * @param {String} data - Data to send
+   */
   async sendByDatagram(data) {
-    await this.datagramWriter.write(this.encoder.encode(data));
-    await this.datagramWriter.write(this.encoder.encode(''));
-  }
-  async sendByStream(data) {
-    await this.streamWriter.write(this.encoder.encode(data));
-    await this.streamWriter.write(this.encoder.encode(''));
+    await this.datagramWriter.write(this.pack(this.encoder.encode(data)));
   }
   /**
-   *
-   * @param {String} data
-   * @param {Boolean} isDatagram
+   * Send data by using stream
+   * @param {String} data - Data to send
+   */
+  async sendByStream(data) {
+    const buffer = this.pack(this.encoder.encode(data));
+    await this.streamWriter.write(buffer);
+  }
+  /**
+   * Send data
+   * @param {String} data - Data to send
+   * @param {Boolean} isDatagram - The data is datagram if true, else stream
    */
   async send(data, isDatagram = false) {
     if (isDatagram) this.sendByDatagram(data);
     else this.sendByStream(data);
+  }
+
+  /**
+   * Add length field to ArrayBuffer
+   * @param {Uint8Array} ary - Length of ary must be less than 65536.
+   */
+  pack(ary) {
+    const len = ary.byteLength;
+    return Uint8Array.from([0,0,0,0,0,0,0,0,0,0,0,0,0,0,Math.floor(len/256),len%256,...ary]);
+  }
+  /**
+   * Get length and data from ArrayBuffer
+   * @param {Uint8Array} ary - Binary data that contains its length
+   */
+  unpack(ary) {
+    const lenField = ary.slice(0,16);
+    for (let i = 0; i < 14; ++i) {
+      if (lenField[i] !== 0) {
+        throw new Error('Invalid length field');
+      }
+    }
+    const len = lenField[14] * 256 + lenField[15];
+
+    const value = ary.slice(16);
+    return [len, value];
   }
 }
